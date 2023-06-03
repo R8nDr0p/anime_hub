4cf2f68ae762f523c8be3ed6a5b7d6adfd8291cb
R8nDr0p <rainchrstn@gmail.com>
2023-06-03 18:52:42 +0800

    enlarge the brandname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ function Header() {
     <>
       <nav className="navbar bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand text-white">Anime Hub</a>
+          <a className="navbar-brand text-white">
+            <h1>Anime Hub</h1>
+          </a>
           <form className="d-flex input-group" role="search">
             <input
               className="form-control me-2"
