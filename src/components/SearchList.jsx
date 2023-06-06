0165f2ed5cbee7713a7a7b63301df33c0aa72853
R8nDr0p <rainchrstn@gmail.com>
2023-06-06 19:32:33 +0800

    added background color

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -10,29 +10,27 @@ function SearchList({ data, searchLoading }) {
     return <h2>No results found.</h2>;
   }
   return (
-    <>
+    <div style={{ backgroundColor: "#121212" }}>
       <div className="container">
         <div className="row">
           <div className="col">
-            <h1>Results:</h1>
+            <h1 style={{ color: "white" }}>Results:</h1>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            {searchLoading ? (
-              <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-              </div>
-            ) : (
-              data.length > 0 &&
-              data.map((item) => {
-                return <Card key={item.mal_id} item={item} />;
-              })
-            )}
-          </div>
+        <div className="row row-cols-sm-1 row-cols-lg-3">
+          {searchLoading ? (
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          ) : (
+            data.length > 0 &&
+            data.map((item) => {
+              return <Card key={item.mal_id} item={item} />;
+            })
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
